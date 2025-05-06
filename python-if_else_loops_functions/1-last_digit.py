18e5a444cf2ece831f819132913a70e9955ad512
dougd0ug <dorine.sgv@hotmail.fr>
2025-05-06 10:39:51 +0200

    fix when its a negative number to kepp the minus

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+copy = number
 if number < 0:
     number = -number
 digit = number % 10
 if digit == 0:
-    print(f"Last digit of {number} is {digit} and is 0")
+    print(f"Last digit of {copy} is {digit} and is 0")
 elif digit > 5:
-    print(f"Last digit of {number} is {digit} and is greater than 5")
+    print(f"Last digit of {copy} is {digit} and is greater than 5")
 else:
-    print(f"Last digit of {number} is {digit} and is less than 6 and not 0")
+    print(f"Last digit of {copy} is {digit} and is less than 6 and not 0")
