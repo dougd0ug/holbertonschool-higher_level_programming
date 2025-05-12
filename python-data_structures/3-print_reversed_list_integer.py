96ac24be8f3dab23f651583e4b1c0bcea61e2f74
dougd0ug <dorine.sgv@hotmail.fr>
2025-05-12 11:08:48 +0200

    fix pycode style

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -3,5 +3,5 @@
 def print_reversed_list_integer(my_list=[]):
     i = len(my_list)
     if my_list:
-        for j in range(i -1, -1, -1):
+        for j in range(i - 1, -1, -1):
             print("{:d}" .format(my_list[j]))
