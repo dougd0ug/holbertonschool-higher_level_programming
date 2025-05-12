d88e5f288894bea903b30cb5abf0299df7e623ed
dougd0ug <dorine.sgv@hotmail.fr>
2025-05-12 10:49:48 +0200

    change the conditions after seeing the task 4, optmise the code

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, element):
-    new_list = my_list
     if idx < 0:
         return (my_list)
     elif idx >= len(my_list):
         return (my_list)
-    elif idx == 0:
-        my_list[0] = element
-        new_list[0] = element
     else:
-        for i in my_list:
-            if i == idx:
-                my_list[i] = element
-                new_list[i] = element
-    return (new_list)
+        my_list[idx] = element
+    return my_list
