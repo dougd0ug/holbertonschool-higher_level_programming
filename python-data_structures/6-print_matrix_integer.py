0e2d191e83ff5578611c126a391ada0d849a46cd
dougd0ug <dorine.sgv@hotmail.fr>
2025-05-12 12:06:57 +0200

    change the condition if the matrix is empty

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -8,3 +8,14 @@ def print_matrix_integer(matrix=[[]]):
                     print("{:d}" .format(i[j]), end=" ")
                 else:
                     print("{:d}" .format(i[j]))
+            print("")
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
