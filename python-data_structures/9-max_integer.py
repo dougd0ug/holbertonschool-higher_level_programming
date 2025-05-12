bccae5f70de058ae5cd50140960801dd5f428449
dougd0ug <dorine.sgv@hotmail.fr>
2025-05-12 14:39:23 +0200

    find the max integer in a list

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    else:
+        result = 0
+        for i in range(len(my_list) - 1):
+            if my_list[i] > result:
+                result = my_list[i]
+    return result
