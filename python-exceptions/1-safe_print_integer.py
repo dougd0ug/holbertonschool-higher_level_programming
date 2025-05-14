7de25e00a6f8780ef5aee3eadf229e41b8fa0a74
dougd0ug <dorine.sgv@hotmail.fr>
2025-05-14 11:44:54 +0200

    add Type Error to the exept

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,8 @@
 def safe_print_integer(value):
     try:
         print("{:d}" .format(value))
-    except (ValueError, TypeError):
-        return False
-    else:
         return True
+    except ValueError:
+        return False
+    except TypeError:
+        return False
