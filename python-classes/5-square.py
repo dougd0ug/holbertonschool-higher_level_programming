8504a55e83655db85021d5b314097d34179cd47a
dougd0ug <dorine.sgv@hotmail.fr>
2025-05-19 15:04:10 +0200

    print the square with the good size

---
+++ python-classes/5-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+5-square.py
+Write an class Square that define a square
+"""
+
+
+class Square:
+    """
+    Class Square
+    """
+    def __init__(self, size=0):
+        """
+        def the arguments and conditions of initialisation
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        def the property
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        def the setter
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        return the area of a square
+        """
+        area_square = self.__size * self.__size
+        return area_square
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+        else:
+            for _ in range(self.size):
+                print("#" * self.size)
