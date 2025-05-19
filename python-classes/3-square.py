eac993ca54e7672ce4ecf85b468bebe5f10ca924
dougd0ug <dorine.sgv@hotmail.fr>
2025-05-19 11:10:57 +0200

    fix: the def of area out of the class

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -20,9 +20,9 @@ class Square:
         else:
             self.__size = size
 
-    def area(self):
-        """
-        return the area of a square
-        """
-        area_square = self.size * 2
-        return area_square
+def area(self):
+    """
+    return the area of a square
+    """
+    area_square = self.size * 2
+    return area_square
