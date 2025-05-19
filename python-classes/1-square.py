2fdd6a1797f3f12bed5dbe029e0b43a7a1f3fb32
dougd0ug <dorine.sgv@hotmail.fr>
2025-05-19 10:46:00 +0200

    define a size in our class

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+1-square.py
+Write an class Square that define a square
+"""
+
+
+class Square:
+    """
+    Class Square
+    """
+    __size = None
+
+    def __init__(self, __size):
+        self.__size = __size
