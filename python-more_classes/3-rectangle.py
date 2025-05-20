e76c90b54a370dc8aaea1619a53b96e799a4fa68
dougd0ug <dorine.sgv@hotmail.fr>
2025-05-20 10:58:03 +0200

    add the magic method __str__

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+"""
+0-rectangle.py
+Write an class Rectangle that define a rectangle
+"""
+
+
+class Rectangle:
+    """
+    Create a class Rectangle
+    """
+    def __init__(self, width=0, height=0):
+        """
+        def the arguments and conditions of initialisation
+        """
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        """
+        Return the rectangle with the character #
+        """
+        if self.width == 0 or self.height == 0:
+            return ""
+        lines = []
+        for _ in range(self.height):
+            lines.append("#" * self.width)
+
+        return "\n".join(lines)
+
+    @property
+    def width(self):
+        """
+        def the property width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        def the setter width
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """
+        def the property height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        def the setter height
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """
+        Return the area of the rectangle
+        """
+        rectangle_area = self.width * self.height
+        return rectangle_area
+
+    def perimeter(self):
+        """
+        Return the perimeter of the rectangle
+        """
+        perimeter_rectangle = 0
+        if self.width == 0 or self.height == 0:
+            perimeter_rectangle = 0
+        else:
+            perimeter_rectangle = ((self.height + self.width) * 2)
+        return perimeter_rectangle
