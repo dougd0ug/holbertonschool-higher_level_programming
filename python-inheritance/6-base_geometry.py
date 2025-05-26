66133e8ead13ac65924e1ddc0a4b239bf5e97439
dougd0ug <dorine.sgv@hotmail.fr>
2025-05-26 15:59:48 +0200

    add def area with an Execption

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+6-base_geometry.py
+Create a class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    Create a class BaseGeometry
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
