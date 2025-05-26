5dce360cb417f0b7de2020f044e46b0616af23c1
dougd0ug <dorine.sgv@hotmail.fr>
2025-05-26 16:20:32 +0200

    add a condition

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -13,5 +13,7 @@ def inherits_from(obj, a_class):
     (directly or indirectly)
     from a_class; otherwise False.
     """
+    if type(obj) is a_class:
+        return False
 
-    return isinstance(obj, a_class) and type(obj) is not a_class
+    return isinstance(obj, a_class)
