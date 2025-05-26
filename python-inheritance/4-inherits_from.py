6b592bbcf7bb82346fa06018f4471311114e8e2e
dougd0ug <dorine.sgv@hotmail.fr>
2025-05-26 15:54:36 +0200

    inherit from a class

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -9,6 +9,9 @@ from the specified class ; otherwise False.
 
 def inherits_from(obj, a_class):
     """
-    Return true is the obj is equal to a_class
+    Returns True if obj is an instance of a class that inherited
+    (directly or indirectly)
+    from a_class; otherwise False.
     """
+
     return isinstance(obj, a_class) and type(obj) is not a_class
