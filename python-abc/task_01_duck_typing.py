6ffa3b3842618d6244f88b02f977b03d9e571512
dougd0ug <dorine.sgv@hotmail.fr>
2025-05-27 14:13:42 +0200

    add desdcription

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -7,19 +7,25 @@ from abc import ABC, abstractmethod
 
 
 class Animal(ABC):
-
+    """
+    ABC Animal class
+    """
     @abstractmethod
     def sound(self):
         pass
 
 
 class Dog(Animal):
-
+    """
+    Dog class inherit from Animal
+    """
     def sound(self):
         return "Bark"
 
 
 class Cat(Animal):
-
+    """
+    Cat class inherit from Animal
+    """
     def sound(self):
         return "Meow"
