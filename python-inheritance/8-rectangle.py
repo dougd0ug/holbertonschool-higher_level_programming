9adb34335ef4c6e950d2e47afc7d16d281dfafcd
dougd0ug <dorine.sgv@hotmail.fr>
2025-05-27 09:54:29 +0200

    add __ for private attribut

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -10,18 +10,18 @@ class BaseGeometry:
     Create a class BaseGeometry
     """
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
-
-    def area(self):
-        raise Exception("area() is not implemented")
+        self.__width = width
+        self.__height = height
 
     def integer_validator(self, name, value):
         if type(value) is not int:
             raise TypeError("{} must be an integer" .format(name))
         elif value <= 0:
             raise ValueError("{} must be greater than 0" .format(name))
-        elif self.height <= 0:
-            raise ValueError("{} must be greater than 0" .format(self.height))
-        elif self.width <= 0:
-            raise ValueError("{} must be greater than 0" .format(self.width))
+        elif self.__height <= 0:
+            raise ValueError("{} must be greater than 0" .format(self.__height))
+        elif self.__width <= 0:
+            raise ValueError("{} must be greater than 0" .format(self.__width))
+
+    def area(self):
+        raise Exception("area() is not implemented")
