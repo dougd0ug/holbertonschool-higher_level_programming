4d0751d45c228646faaa2bb743d751f8abf9e69e
dougd0ug <dorine.sgv@hotmail.fr>
2025-05-28 09:36:02 +0200

    create abc class with append, extend, remove, pop

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -9,18 +9,18 @@ class VerboseList(list):
     ABC VerboseList class
     """
     def append(self, item):
-        super.append(item)
+        super().append(item)
         print("Added [{}] to the list." .format(item))
 
     def extend(self, item):
-        super.append(item)
+        super().append(item)
         print("Extended the list with [{}] items." .format(len(item)))
 
     def remove(self, item):
-        super.append(item)
+        super().append(item)
         print("Removed [{}] from the list." .format(item))
 
     def pop(self, item):
-        item = super.append(item)
+        item = super().append(item)
         print("Popped [{}] from the list." .format(item))
         return item
