469a5238182a6593be66288e8b8e66271e8f935a
dougd0ug <dorine.sgv@hotmail.fr>
2025-06-02 16:56:50 +0200

    return the JSON representation of an object

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+3-to_json_string.py
+returns the JSON representation of an object
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    returns the JSON representation of an object
+    """
+    return json.dumps(my_obj)
