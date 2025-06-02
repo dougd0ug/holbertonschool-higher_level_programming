a23519431b339481a29c767e6dd895162cc095d7
dougd0ug <dorine.sgv@hotmail.fr>
2025-06-02 10:06:50 +0200

    create a function to read a file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+0-read_file.py
+Read a file
+"""
+
+
+def read_file(filename=""):
+    """
+    Function for read a file
+    """
+    with open(filename, encoding="utf-8") as f:
+        list(f)
