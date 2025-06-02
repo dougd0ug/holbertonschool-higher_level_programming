859950ad060543a46e9c9e411f4f9c0469a83d84
dougd0ug <dorine.sgv@hotmail.fr>
2025-06-02 16:40:14 +0200

    add w

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -9,5 +9,5 @@ def write_file(filename="", text=""):
     """
     Function for read a file
     """
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         f.write(text)
