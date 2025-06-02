4a658499d3f58cfee8dd2221779fc2e9395d1396
dougd0ug <dorine.sgv@hotmail.fr>
2025-06-02 17:03:00 +0200

    return an object represented by SJON string

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -8,6 +8,6 @@ import json
 
 def from_json_string(my_str):
     """
-     returns an object (Python data structure) represented by a JSON string
+    returns an object (Python data structure) represented by a JSON string
     """
-    return json.dump(my_str)
+    return json.loads(my_str)
