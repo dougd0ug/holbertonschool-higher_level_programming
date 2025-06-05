294527edbd012f1867d9857a8d865794457152ca
dougd0ug <dorine.sgv@hotmail.fr>
2025-06-05 14:22:40 +0200

    create a class Student

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -10,9 +10,9 @@ class Student:
     class Student
     """
     def __init__(self, first_name, last_name, age):
-        first_name.self = first_name
-        last_name.self = last_name
-        age.self = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
-        return Student.__dict__
+        return self.__dict__
