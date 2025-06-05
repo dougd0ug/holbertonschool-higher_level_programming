1a7d62f9760f1ceb68f3011266474c02fd6afbef
dougd0ug <dorine.sgv@hotmail.fr>
2025-06-05 14:16:09 +0200

    write de class Student

---
+++ python-input_output/9-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+8-student.py
+write a class Student
+"""
+
+
+class Student:
+    """
+    class Student
+    """
+    def __init__(self, first_name, last_name, age):
+        first_name.self = first_name
+        last_name.self = last_name
+        age.self = age
+
+    def to_json(self):
+        return Student.__dict__
