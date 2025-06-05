0c7abdf62c7dd6f22180acfde08eb73da197134c
dougd0ug <dorine.sgv@hotmail.fr>
2025-06-05 14:05:50 +0200

    return a dictionnary

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+8-class_to_json.py
+function that returns the dictionary description
+"""
+
+
+import json
+
+
+def class_to_json(obj):
+    """
+    function that returns the dictionary description
+    """
+    return obj.__dict__
