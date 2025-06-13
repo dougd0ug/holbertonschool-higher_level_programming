ed7a75d04e3fea7c44675acb85374e7b0168690b
dougd0ug <dorine.sgv@hotmail.fr>
2025-06-13 09:44:08 +0200

    create a server with handle errors

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,40 @@
+import http.server
+import socketserver
+import json
+
+
+class Server(http.server.BaseHTTPRequestHandler):
+    """
+    Class to handle the path and errors
+    """
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+
+        elif self.path == '/data':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(data).encode())
+
+        elif self.path == '/status':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"OK")
+
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"404 Not Found: The endpoint doesn't exist.")
+
+
+PORT = 8000
+with socketserver.TCPServer(("", PORT), Server) as httpd:
+    print("Serving on port {}" .format(PORT))
+    httpd.serve_forever()
