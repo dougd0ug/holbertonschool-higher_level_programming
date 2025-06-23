e286444ca75c2c3680e2128b82cb56bf2461a3a2
dougd0ug <dorine.sgv@hotmail.fr>
2025-06-23 15:19:55 +0200

    add GROUP BY

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,4 @@
 -- List the number of records with the same score
-SELECT score, COUNT(score) as number FROM second_table WHERE score = score ORDER BY score DESC;
+SELECT score, COUNT(score) as number
+FROM second_table
+GROUP BY score ORDER BY score DESC;
