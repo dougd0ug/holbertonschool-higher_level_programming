0183d83575245a202cbaf14494c3fd0dfa1d2cd8
dougd0ug <dorine.sgv@hotmail.fr>
2025-06-24 11:29:02 +0200

    delete the underscore

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- Create a database and a user
-CREATE USER IF NOT EXISTS 'user_0d_2'@'local_host' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'local_host';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
