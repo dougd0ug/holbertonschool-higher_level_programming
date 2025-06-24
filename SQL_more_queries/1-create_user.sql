08d785eda005c1c51b51b8e51ec3f402817c107d
dougd0ug <dorine.sgv@hotmail.fr>
2025-06-24 10:52:37 +0200

    grant all privileges

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- create a user and give all privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT SELECT
+GRANT ALL PRIVILEGES
 ON *.*
 TO 'user_0d_1'@'localhost';
