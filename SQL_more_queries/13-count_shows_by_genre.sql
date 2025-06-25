83134db2af4a15f71022c7ae165d8e01f4fd5d03
dougd0ug <dorine.sgv@hotmail.fr>
2025-06-25 12:52:31 +0200

    count shows by genre

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 --  lists all shows contained in the database hbtn_0d_tvshows
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_shows
-FROM tv_shows
+FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_show_genres.genre_id IS NOT NULL
 GROUP BY genre
