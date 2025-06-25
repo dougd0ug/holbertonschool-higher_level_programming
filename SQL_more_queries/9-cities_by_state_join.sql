e6f3a2582f4a73f1ed8aba98ba7f6922b70c0153
dougd0ug <dorine.sgv@hotmail.fr>
2025-06-25 11:24:25 +0200

    change the order of the request

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- lists all cities contained in the database hbtn_0d_usa
 SELECT cities.id, cities.name, states.name
-FROM cities
-INNER JOIN states on cities.states_id = states.id
+FROM states
+INNER JOIN cities on states.id = cities.states_id
 ORDER BY cities.id ASC;
