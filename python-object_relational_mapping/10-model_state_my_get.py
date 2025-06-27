e61f933e27db02cf7658a153c076ec8045443088
dougd0ug <dorine.sgv@hotmail.fr>
2025-06-27 15:17:49 +0200

    fix the query

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,40 +1,25 @@
 #!/usr/bin/python3
 """
-Module containing function displaying objects from a database.
+Module that contains
 """
-import sys
+from sys import argv
 from model_state import Base, State
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+if __name__ == "__main__":
+    username, password, dbname, search_name = argv[1], argv[2], argv[3], argv[4]
+    db_url = f"mysql+mysqldb://{username}:{password}@localhost:3306/{dbname}"
 
-def main():
-    """
-    Lists first State object from a database.
-    """
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(
-            username,
-            password,
-            database
-        ),
-        pool_pre_ping=True
-    )
-    Base.metadata.create_all(engine)
+    engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
-
     session = Session()
+
     state = session.query(State).filter(State.name == search_name).first()
+
     if state:
         print(state.id)
     else:
         print("Not found")
 
-
-if __name__ == "__main__":
-    main()
+    session.close()
