873f27a7e4e8681da9f139d3e4fea7bc0d3adf3a
dougd0ug <dorine.sgv@hotmail.fr>
2025-06-27 12:43:28 +0200

    display all values

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
 """
-Lists all values in the states table where name matches the argument,
-safe from SQL injection.
+script that takes in an argument and displays all values in the states table
 """
-
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Get arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
     state_name = sys.argv[4]
 
-    # Connect to the database
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -22,12 +18,12 @@ if __name__ == "__main__":
         passwd=password,
         db=database
     )
-
     cur = db.cursor()
-
-    # Secure query using parameterized inputs
-    query = "SELECT * FROM states WHERE name = BINARY %s ORDER BY id ASC"
-    cur.execute(query, (state_name,))
+    query = (
+        "SELECT * FROM states WHERE name = '{}' "
+        "ORDER BY id ASC".format(state_name)
+    )
+    cur.execute(query)
 
     for row in cur.fetchall():
         print(row)
