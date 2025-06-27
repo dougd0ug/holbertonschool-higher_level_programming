2067a7502ff4716fe92a96e7b5790f09e071557e
dougd0ug <dorine.sgv@hotmail.fr>
2025-06-27 12:42:19 +0200

    file executable

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 """
-Lists all values in the states table where name matches the argument.
+Lists all values in the states table where name matches the argument,
+safe from SQL injection.
 """
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-
+    # Get arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
     state_name = sys.argv[4]
 
+    # Connect to the database
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -23,13 +25,9 @@ if __name__ == "__main__":
 
     cur = db.cursor()
 
-    query = (
-        "SELECT * FROM states "
-        "WHERE name = BINARY '{}' "
-        "ORDER BY id ASC".format(state_name)
-    )
-
-    cur.execute(query)
+    # Secure query using parameterized inputs
+    query = "SELECT * FROM states WHERE name = BINARY %s ORDER BY id ASC"
+    cur.execute(query, (state_name,))
 
     for row in cur.fetchall():
         print(row)
