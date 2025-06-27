9ea0c7c1527d131cc9df1912e2a85c28b8d529f6
dougd0ug <dorine.sgv@hotmail.fr>
2025-06-27 12:38:03 +0200

    safe filter to avoir SQL injection

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 """
-Lists all values in the states table where name matches the argument,
-safe from SQL injection.
+script that takes in arguments and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument. But this time,
+write one that is safe from MySQL injections!
 """
-
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
     state_name = sys.argv[4]
+
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -20,11 +20,11 @@ if __name__ == "__main__":
         passwd=password,
         db=database
     )
-
     cur = db.cursor()
-
-    query = "SELECT * FROM states WHERE name = BINARY %s ORDER BY id ASC"
-    cur.execute(query, (state_name,))
+    cur.execute(
+        "SELECT * FROM states WHERE name = %s "
+        "ORDER BY id ASC", (state_name,)
+    )
 
     for row in cur.fetchall():
         print(row)
