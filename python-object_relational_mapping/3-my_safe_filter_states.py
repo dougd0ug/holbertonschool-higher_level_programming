b0d569d89353cc3f7aa89ce25fdd5badb61a4b10
dougd0ug <dorine.sgv@hotmail.fr>
2025-06-27 12:34:21 +0200

    safe filter to avoir SQL injection

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Lists all values in the states table where name matches the argument,
+safe from SQL injection.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Get arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to the database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cur = db.cursor()
+
+    # Secure query using parameterized inputs
+    query = "SELECT * FROM states WHERE name = BINARY %s ORDER BY id ASC"
+    cur.execute(query, (state_name,))
+
+    for row in cur.fetchall():
+        print(row)
+
+    cur.close()
+    db.close()
