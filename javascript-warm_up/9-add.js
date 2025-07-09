1d4e893105410c37db9b34dbc7381bc01fe49df4
dougd0ug <dorine.sgv@hotmail.fr>
2025-07-09 22:47:33 +0200

    create add function

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+function add(a, b) {
+    console.log(a + b);
+}
+
+const firstArg = Number(process.argv[2]);
+const secondArg = Number(process.argv[3]);
+
+add(firstArg, secondArg);
